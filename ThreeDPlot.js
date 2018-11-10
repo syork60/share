937b8e426e2b738d00f0706ab4334938d3e53952
SteveAYork@yahoo.com <SteveAYork@yahoo.com>
2018-11-09 20:08:07 -0600

    Added dispose to ThreeDPlot

--- ThreeDPlot.js
+++ ThreeDPlot.js
@@ -96,4 +96,46 @@ var ThreeDPlot=function(xExtent,yExtent,zExtent,divSize,xPlaneName,zPlaneName) {
 		var rc=new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000);
 		rc.position.set(-this.maxX*1.4, this.maxY*1.3, this.maxX*2); return rc;
 	}
+
+	function disposeNode (node)
+	{
+		if (node instanceof THREE.Mesh)
+		{
+			if (node.geometry) node.geometry.dispose (); 
+			if (node.material)
+			{
+				if (node.material instanceof THREE.MeshFaceMaterial || node.material instanceof THREE.MultiMaterial)
+				{
+					$.each (node.material.materials, function (idx, mtrl) {
+						if (mtrl.map)           mtrl.map.dispose ();
+						if (mtrl.lightMap)      mtrl.lightMap.dispose ();
+						if (mtrl.bumpMap)       mtrl.bumpMap.dispose ();
+						if (mtrl.normalMap)     mtrl.normalMap.dispose ();
+						if (mtrl.specularMap)   mtrl.specularMap.dispose ();
+						if (mtrl.envMap)        mtrl.envMap.dispose ();
+						mtrl.dispose ();    // disposes any programs associated with the material
+					});
+				}
+				else
+				{
+					if (node.material.map)          node.material.map.dispose ();
+					if (node.material.lightMap)     node.material.lightMap.dispose ();
+					if (node.material.bumpMap)      node.material.bumpMap.dispose ();
+					if (node.material.normalMap)    node.material.normalMap.dispose ();
+					if (node.material.specularMap)  node.material.specularMap.dispose ();
+					if (node.material.envMap)       node.material.envMap.dispose ();
+					node.material.dispose ();   // disposes any programs associated with the material
+				}
+			}
+		}
+		node.parent.remove(node);
+	}   // disposeNode
+
+	function intDispose(node,callback)
+	{
+		for (var i=node.children.length-1;i>=0;i--)
+			{ var child=node.children[i]; intDispose(child,callback); callback (child); }
+	}
+
+	this.dispose=function(scene) { if (scene) intDispose(scene,disposeNode); }
 };
